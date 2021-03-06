2ae4443981977c034bfc87c90abfeb872733d38e
asiimwedismas <asiimwedismas@gmail.com>
2021-03-06 12:54:34 +0300

    Initial commit

---
+++ src/main/java/com/asiimwedismas/birddiary/BirdDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.asiimwedismas.birddiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdDiaryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BirdDiaryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/asiimwedismas/birddiary/BirdDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiimwedismas.birddiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdDiaryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
